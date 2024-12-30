b919ef48881d739ab6475a0c4827b036214d6378
kareulo <jcajuab@tutamail.com>
2024-12-30 17:52:21 +0800

    feat: remove leading newline in getRobotsTxt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,11 +1,12 @@
 import type { APIRoute } from "astro"
 
-const getRobotsTxt = (sitemapURL: URL) => `
-User-agent: *
+function getRobotsTxt(sitemapURL: URL) {
+  return `User-agent: *
 Disallow:
 
 Sitemap: ${sitemapURL.href}
 `
+}
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL("sitemap-index.xml", site)
